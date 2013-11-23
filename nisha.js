f749c283b2778e95b5d4b3ddd4c1c3f0485feb49
pyprism <areose21@gmail.com>
2013-11-23 19:53:26 +0600

    add nisha.js

---
+++ nisha.js
@@ -0,0 +1,11 @@
+var paste = require('./pastebin.js');
+
+var key = "ur";
+var code = "test from nodejs";
+
+//var apipas = "paste";
+
+//var name = "hirenTest" ;
+
+var x = paste.newPaste(key,code),function(result)};
+console.log(x);
